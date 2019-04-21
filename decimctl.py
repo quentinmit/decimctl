2fc57a098255caadca635141e32ac35da1db5c69
Quentin Smith <quentin@mit.edu>
2019-04-21 02:32:49 -0400

    Better structure

--- decimctl.py
+++ decimctl.py
@@ -2,6 +2,7 @@
 
 import os
 import datetime
+from struct import pack
 from itertools import chain
 from functools import reduce
 import pylibftdi.driver
@@ -11,6 +12,8 @@ import pylibftdi.device
 pylibftdi.device.USB_VID_LIST = pylibftdi.driver.USB_VID_LIST
 pylibftdi.device.USB_PID_LIST = pylibftdi.device.USB_VID_LIST
 from ctypes import byref
+
+import protocol
 
 print (pylibftdi.driver.Driver().list_devices())
 
@@ -50,25 +53,59 @@ class Decimator(pylibftdi.device.Device):
             self.log_file.write(bytes(bytearray(chain.from_iterable(zip(data_in, out)))))
         return out
 
-    @staticmethod
-    def _raw_to_bytes(raw):
-        status_bits = []
-        for i in range(0, len(raw), 4):
-            if raw[i+2] != raw[i+3]:
-                print ("difference at bit", i)
-            status_bits.append(bool(raw[i+2] & 0x8))
-        status_bytes = bytearray(
-            reduce(lambda a, b: (a << 1) | b, (int(x) for x in byte_bits))
-            for byte_bits
-            in zip(*[iter(status_bits)]*8)
-        )
-        #print (bitstr, b, chr(int(bitstr, 2)))
-        return bytes(status_bytes)
-
     def read_bytes(self, n):
+        # Toggle the clock line once for each bit.
         data = b'\x00\x40\x40\x00'*4096
         raw = dev.clock_raw_bytes(data)
-        return self._raw_to_bytes(raw)
+        return protocol.raw_response_to_bytes(raw)
+
+    def write_bytes(self, command, postamble=b''):
+        # \x00\x01 = get status
+        # \x00\x62\x00 + (raw)\x00\x40\x48 = set SDI OUT to SDI IN
+        # \x00\x62\x02 + (raw)\x00\x40\x48 = set SDI OUT to SCALER
+        data = protocol.COMMAND_PREAMBLE + protocol.bytes_to_raw_command(command) + postamble
+        return self.clock_raw_bytes(data)
+
+    def fpga_write_bytes(register, value):
+        # @TUSB@FPGA_WRITE_BYTES$q ui puc ui ui
+        # CPA_SO_Source calls @TUSB@FPGA_WRITE_BYTES(0, &value, 0x31, 0x1) where value is 0->3
+
+        buf = bytearray(protocol.COMMAND_PREAMBLE)
+
+        address = (register<<1) & 0xfffe
+
+        buf.extend(protocol.bytes_to_raw_command(pack(">H", address)))
+
+        buf.extend(protocol.bytes_to_raw_command(value))
+
+        buf.extend(protocol.COMMAND_POSTAMBLE)
+
+        return self.clock_raw_bytes(bytes(buf))
+
+    def fpga_read_bytes(register_start, length):
+
+        buf = bytearray(protocol.READ_PREAMBLE)
+
+        buf.extend(protocol.bytes_to_raw_command(pack(">H", address)))
+
+        self.clock_raw_bytes(bytes(buf))
+
+        return self.read_bytes(length)
+        
+
+    def set_SO_Source(self, source):
+        # CPA_SO_Source = register 0x31, & 0x3
+        self.fpga_write_bytes(CPA_SO_Source, pack('>B', source & 0x3))
+
+    CPA_HO_Type = 0x30 # & 0x7
+    CPA_SO_Source = 0x31 # & 0x3
+    CPA_HO_Source = 0x32 # & 0x3
+    CPA_DUC_Source = 0x33 # bottom bit = SDI/HDMI as source, top 3 bits = Source, Free Run, SDI IN, HDMI IN
+    CPA_LCDOffTime = 0x35 # & 0x7
+    # 5s, 15s, 30s, 1m, 5m, 10m, 30m, NEVER
+    CPA_Loop_Enable = 0x2b # & 0x1
+    CPA_DUCFormat = 0x22 # & 0x1f
+    
 
 with Decimator(log_raw_data=True) as dev:
     # FT_ResetDevice()
